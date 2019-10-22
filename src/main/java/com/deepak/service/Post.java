312db0f4be6fe02b6711ba864483a7fb48f91de0
Joaquin Alberto Huauya Arontico <jhuauyaa@everis.com>
2019-10-22 16:07:02 -0500

    first commmit

---
+++ src/main/java/com/deepak/service/Post.java
@@ -0,0 +1,18 @@
+package com.deepak.service;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class Post {
+    
+    Integer userId;
+    
+    Integer id;
+    
+    String title;
+    
+    String body;
+
+}
